82444d6bbc43d5ffc51d28927cc9fa36a2937263
Jackson Porter <readjacksonporter@gmail.com>
2022-07-20 14:36:33 -0600

    second second push

---
+++ frontEnd/main.js
@@ -0,0 +1,7 @@
+let button = document.querySelector('button')
+
+const buttonAlert = () => {
+    alert('Apple would not allow characters to use iPhones if they were a villain in the film.')
+}
+
+button.addEventListener('click', buttonAlert)
---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendfile(path.join(__dirname, '../frontEnd/index.html'))
+})
+
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontEnd/styles.css'))
+})
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontEnd/main.js'))
+})
+
+const port = process.env.PORT || 4005
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
